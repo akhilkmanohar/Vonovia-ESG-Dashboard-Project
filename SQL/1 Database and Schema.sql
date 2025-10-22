a6c48d818075eb3eefde8e7e3ef84008b5e8994e
akhilkmanohar <akhilkmanohar@hotmail.com>
2025-10-22 19:02:11 +0200

    feat(sql): add database and schema creation script
    Create Vonovia_ESG_DB with stg, core, and mart schemas for ESG data

---
+++ SQL/1 Database and Schema.sql
@@ -0,0 +1,14 @@
+
+/* 1) Create database  */
+
+ CREATE DATABASE Vonovia_ESG_DB;
+
+ USE Vonovia_ESG_DB;
+
+ /* 3) Create schemas */
+
+ CREATE SCHEMA stg
+
+ CREATE SCHEMA core
+
+ CREATE SCHEMA mart
